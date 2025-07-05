19354218a826c7f2a07d8732c82f6079aa7d85f3
Victor Rosa <vicrosa25@gmail.com>
2025-07-05 12:44:19 +0200

    Created Folder Structure

---
+++ src/main/java/rosa/victor/pruebainditex/PruebaInditexApplication.java
@@ -0,0 +1,13 @@
+package rosa.victor.pruebainditex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaInditexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaInditexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rosa/victor/pruebainditex/PruebaInditexApplicationTests.java
@@ -0,0 +1,13 @@
+package rosa.victor.pruebainditex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaInditexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
